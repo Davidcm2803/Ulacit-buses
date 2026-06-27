f66049b741ba87bd6a915ae3351dc6ac7e858b38
David Castillo Mora <davidcm2803@gmail.com>
2026-06-27 12:30:31 -0600

    lm

--- backend/test_firebase.py
+++
@@ -1,8 +0,0 @@
-from app.services.firebase_service import initialize_firebase
-
-
-try:
-    initialize_firebase()
-    print("Firebase Admin conectado correctamente")
-except Exception as error:
-    print(f"Error inicializando Firebase: {error}")
