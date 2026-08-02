e9ab4addfb97bcf380a04441d3bf386aedeac29e
David Castillo Mora <davidcm2803@gmail.com>
2026-08-01 19:25:40 -0600

    app

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,5 +1,4 @@
-from fastapi import FastAPI, Request
-from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI, Request, APIRouter
 from fastapi.responses import Response
 from mangum import Mangum
 
@@ -10,6 +9,7 @@ from app.Mongo.indexes import crear_indices
 from app.routes.auth_routes import router as auth_route
 from app.services.firebase_service import initialize_firebase
 from app.routes.payments import router as payments_router
+from app.routes.tickets import router as tickets_router
 
 app = FastAPI(
     title="Ulacit Buses API",
@@ -23,36 +23,25 @@ async def add_coop_header(request: Request, call_next):
     response.headers["Cross-Origin-Opener-Policy"] = "same-origin-allow-popups"
     return response
 
-origins = [
-    "http://localhost:5174",
-    "http://localhost:5173",
-    "aca el de aws",
-]
+api_router = APIRouter(prefix="/api")
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+api_router.include_router(history_router)
+api_router.include_router(auth_route)
+api_router.include_router(routes_router)
+api_router.include_router(stops_router)
+api_router.include_router(payments_router)
+api_router.include_router(tickets_router)
 
-app.include_router(history_router)
-app.include_router(auth_route)
-app.include_router(routes_router)
-app.include_router(stops_router)
-app.include_router(payments_router)
+@api_router.get("/health")
+async def health():
+    return {"status": "ok"}
 
-
+app.include_router(api_router)
 
 @app.on_event("startup")
 def on_startup():
     initialize_firebase()
     crear_indices()
-
-@app.get("/health")
-async def health():
-    return {"status": "ok"}
 
 # Entry point para AWS Lambda
 handler = Mangum(app)
